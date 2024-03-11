f204894dc0a3b5686e3d96014bb470ccf9440687
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-11 21:56:41 +0300

    Organize routes, and clean up code

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>This is dashboard (Protected)</div>;
+};
+
+export default Page;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>This is Landing Page (Unprotected)</div>;
+};
+
+export default Page;
